4011bc98a87fa919f483e8848a45a25bea2846dd
Chris Piemonte <chrispiemonte@chriss-air.lan>
2019-01-30 18:13:20 -0500

    form measurement logic on selected activity

--- src/app/challenges/create-challenge/create-challenge.component.ts
+++ src/app/challenges/create-challenge/create-challenge.component.ts
@@ -2,6 +2,11 @@ import { Component, OnInit } from '@angular/core';
 import { Location } from '@angular/common';
 import { FormGroup, FormControl } from '@angular/forms';
 
+export interface Measurement {
+  view: string;
+  activities: string[];
+}
+
 @Component({
   selector: 'app-create-challenge',
   templateUrl: './create-challenge.component.html',
@@ -11,11 +16,16 @@ export class CreateChallengeComponent implements OnInit {
 
   constructor(private location: Location) { }
 
-  measurements = ['Steps', 'Time', 'Miles', 'Total Weight']
+  measurements: Measurement[] = [
+    {view: 'Steps', activities: ['walking']},
+    {view: 'Time', activities: ['walking', 'running', 'biking', 'swimming']},
+    {view: 'Miles', activities: ['walking', 'running', 'biking']},
+    {view: 'Weight', activities: ['weight']}
+  ]
 
   form = new FormGroup({
     activity: new FormControl(''),
-    measurement: new FormControl(''),
+    measurement: new FormControl({value: '', disabled: true}),
     duration: new FormControl(''),
     invitees: new FormControl('')
   })
@@ -23,19 +33,36 @@ export class CreateChallengeComponent implements OnInit {
   ngOnInit() {
   }
 
-  formIsEmpty() {
+  validForActivity(activity: String, measurement: []) {
+    let valid = false
+    if(activity) {
+      measurement.forEach(measurement => {
+        if(measurement === activity) {valid = true}
+      })
+    }
+    return valid
+  }
+
+  checkForActivity(): void {
+    if(this.form.value.activity.length > 0) {
+      this.form.controls.measurement.enable()
+    }
+  }
+
+  formIsEmpty(): Boolean {
     if(this.form.value.activity || this.form.value.measurement || this.form.value.duration || this.form.value.invitees) {
-      return this.form.value.activity.length < 1 || this.form.value.measurement.length < 1 || this.form.value.duration.length < 1 || this.form.value.invitees.length < 1
+      return this.form.value.activity.length < 1 && this.form.value.measurement.length < 1 && this.form.value.duration.length < 1 && this.form.value.invitees.length < 1
     } else {
       return true
     }
   }
 
-  clear() {
+  clear(): void {
     this.form.reset()
+    this.form.controls.measurement.disable()
   }
 
-  goBack() {
+  goBack(): void {
     this.location.back()
   }
 }
