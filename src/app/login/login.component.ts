b39faca9306ff622b6f555a9e785e0f7dce20478
Robert <boespflr@oregonstate.edu>
2019-02-20 12:18:37 -0800

    login and regisration forms validated for valid input and complete form

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -10,19 +10,33 @@ import { AuthService } from '../services/auth.service';
 import { DatabaseService } from '../services/database.service';
 import { UserService } from '../services/user.service';
 import { Router } from '@angular/router';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatButtonModule, MatInputModule, MatProgressBarModule } from '@angular/material';
+import { ReactiveFormsModule, FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css']
 })
+
 export class LoginComponent implements OnInit {
+
+  loginForm: FormGroup;
 
   constructor (
     private authService: AuthService,
     private dbService: DatabaseService,
     private userService: UserService,
-    private router: Router
+    private router: Router,
+    private browserAnimationModule: BrowserAnimationsModule,
+    private matbuttonModule: MatButtonModule,
+    private matInputModule: MatInputModule,
+    private reactiveFormsModule: ReactiveFormsModule,
+    private matProgressBarModule: MatProgressBarModule,
+    private fb: FormBuilder,
+    // private validators: Validators
+    // private fg: FormGroup
   ) { }
 
   user: User = new User;
@@ -31,14 +45,48 @@ export class LoginComponent implements OnInit {
 
   //runs when component loads
   ngOnInit() {
+    //if user hit remember me on last session at login,
+    //loads user and navigates to home page skipping login
     this.authService.loadRememberedUser();
+
+    //form group controls form fields
+    this.loginForm = this.fb.group({
+      email: ['', [
+        Validators.required,
+        Validators.email
+      ]],
+      password: ["",[
+        Validators.required,
+        Validators.minLength(6)
+      ]],
+      rememberUser: [false, [
+        Validators.requiredTrue
+      ]]
+    });
+  }
+
+  //these getters make the html cleaner for error messages
+  //under <mat-error> tag
+  get email() {
+    return this.loginForm.get('email');
+  }
+
+  get password() {
+    return this.loginForm.get('password');
   }
 
   //logs in user in with email and password
   login() {
 
-    //user hit submit btn
+    //hides submit and buttons so user can't
+    //hit log in btn more than once
     this.submitted = true;
+
+    //fill user object based on form values and remember me
+    this.user.username = this.loginForm.value.email;
+    this.user.password = this.loginForm.value.password;
+    this.rememberUser =  this.loginForm.value.rememberUser;
+    // console.log(this.user);
 
     //sets remember me in auth service
     this.authService.setRememberMe(this.rememberUser);
