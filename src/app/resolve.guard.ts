9e355f69d583bf347d36727e8a81a45531f2bb28
Robert <boespflr@oregonstate.edu>
2019-03-10 18:15:09 -0700

    user resolve guard working on view-activity

---
+++ src/app/resolve.guard.ts
@@ -0,0 +1,33 @@
+import { of, Observable } from 'rxjs';
+import { map, catchError } from 'rxjs/operators';
+import { Injectable } from '@angular/core';
+import { CanActivate, Router } from '@angular/router';
+import { DatabaseService } from './services/database.service';
+import { AuthService } from './services/auth.service';
+import { UserService } from './services/user.service';
+import { Observer } from 'firebase';
+
+@Injectable()
+export class ResolveGuard implements CanActivate {
+constructor(
+    private dbService: DatabaseService,
+    private authService: AuthService,
+    private userService: UserService
+){}
+
+//https://shermandigital.com/blog/wait-for-data-before-rendering-views-in-angular-2/
+//https://stackoverflow.com/questions/37897273/authguard-doesnt-wait-for-authentication-to-finish-before-checking-user
+//https://stackoverflow.com/questions/50203241/angular-5-to-6-upgrade-property-map-does-not-exist-on-type-observable
+//
+canActivate(): Observable<boolean> {
+    let remembered: string = localStorage.getItem('loggedIn');
+    if(this.authService.isLoggedIn() && remembered) { 
+      return this.dbService.getUser(JSON.parse(remembered).id)
+      .pipe(map(
+          data => {
+              this.userService.setCurrentUserId(data['_id']);
+              this.userService.setToken(JSON.parse(remembered).token);
+              return true;
+          }
+      ))
+}}}
